a87d5bd7e04ec70ade0a67fc1aad10a37dd1e2e9
junaid <jk7948677@gmail.com>
2026-02-25 12:18:54 +0530

    Added the user prompt

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-console.log("yelo")
-
-
-let getUserChoice
 
 function getComputerChoice() {
     let result = Math.floor(Math.random() * 3) + 1;
-    
+
     if (result === 1){
         return "rock"
     } else if (result === 2){
@@ -15,4 +11,21 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let userChoice = +(prompt("Choose the number :\n rock (1)\n paper (2)\n scissors (3)"));
+
+    if (userChoice === 1) {
+        return "rock"
+    } else if (userChoice === 2) {
+        return "paper"
+    } else if (userChoice === 3) {
+        return "scissors"
+    } else {
+        return alert("Invalid choice")
+    }
+
+    
+}
+
 console.log(getComputerChoice())
+console.log(getHumanChoice())
